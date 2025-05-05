9c6472bafb2adc32dd9b806c3faa4647b12212bb
Aayush Bajaj <aayushbajaj7@gmail.com>
2025-05-05 22:50:50 +1000

    factorial. dp?

---
+++ fact.lua
@@ -0,0 +1,12 @@
+-- comment!
+function fact (n)
+    if n == 0 then
+        return 1
+    else
+        return n * fact(n - 1)
+    end
+end
+
+print("enter a number:")
+a = io.read("*n") -- pointer syntax?
+print(fact(a))
